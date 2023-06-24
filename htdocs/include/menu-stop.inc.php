cb1c0c5a2726003025e841af464f0013010e3d87
bhuism <bhuism@odee.net>
2023-06-25 00:23:01 +0200

    add href

--- htdocs/include/menu-stop.inc.php
+++ htdocs/include/menu-stop.inc.php
@@ -8,7 +8,7 @@
 <tr><td colspan="3"><img src="/images/pixel.gif" width="1" height="1" alt=""></td></tr>
 <tr>
 <td align=left valign=top><font color="#B0B0B0"><small>&nbsp;<?php echo date('r', time()); ?></small></font></td>
-<td valign=top><font color="#B0B0B0"><small>@@@GITHASH@@@</small></font></td>
+<td valign=top><font color="#B0B0B0"><small><a href="https://github.com/bhuism/appsource/commit/@@@GITHASH@@@">@@@GITHASH@@@</a></small></font></td>
 <td align=right valign=top><font color="#B0B0B0"><small>&copy; <?php echo date('r', time()); ?> Appsource&nbsp;</small></font></td>
 </tr>
 <tr><td colspan="3"><img src="/images/pixel.gif" width="1" height="1" alt=""></td></tr>
